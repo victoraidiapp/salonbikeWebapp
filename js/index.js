0311676995a34c8f2eac910f3d69a48efd4e9111
aidiapp2013 <victor@aidiapp.com>
2014-09-21 21:59:43 +0200

    Estructura de clases
    Incorporo los archivos que conformarán la estructura de clases

--- js/index.js
+++ js/index.js
@@ -19,13 +19,13 @@
 var app = {
     // Application Constructor
     initialize: function() {
-        this.bindEvents();
+        this.manejadores();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    manejadores: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
---
+++ js/mapmanager.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+var MapManager={
+	
+	//Esta función inicializa y pinta el mapa Google, para ello recibe el identificador del contenedor donde pintar el mapa
+	init:function(mapId){
+		
+	}
+	
+	
+}

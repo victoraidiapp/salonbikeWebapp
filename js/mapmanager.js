52b17b32bf006cc3a694c0e4be15fc9209a91015
aidiapp2013 <victor@aidiapp.com>
2014-09-26 18:15:24 +0200

    Preparo MapManager
    Preparo MapManager para pintar los carriles bici en el mapa

--- js/mapmanager.js
+++ js/mapmanager.js
@@ -1,11 +1,14 @@
 // JavaScript Document
-var bs,bl; 
+
 
 var MapManager={
 	//En este objeto almacenaremos el mapa de Google.
 	flagStationLayer:false,
 	flagLanesLayer:false,
 	mapObject:null,
+	BikeLanes:null,
+	BikeLanesPolyLine:new Array(),//Este array contiene una colección de google.maps.Polyline que luego se pintan en el mapa con el método setMap
+	BikeStations:null,
 	//Esta función inicializa y pinta el mapa Google, para ello recibe el identificador del contenedor donde pintar el mapa
 	init:function(mapId){
 		var infoWindow=new google.maps.InfoWindow();
@@ -25,6 +28,8 @@ var MapManager={
 	//Esta función mostrará la capa con los intercambiadores
 	showBikeStationLayer:function(){
 		
+			
+		
 	},
 	//Esta función ocultará la capa con los intercambiadores,
 	hideBikeStationLayer:function(){
@@ -32,13 +37,36 @@ var MapManager={
 	},
 	//Esta función mostrará la capa con los carriles bici
 	showBikeLaneLayer:function(){
+		
+		
 		LoadingDialog.show("Cargando contenido");
-		DataManager.getBikeLanes(function(b){
+		if(this.BikeLanesPolyLine.length>0){//Si ya tenemos las lineas guardadas, simplemente las pintamos
+		for(l in this.BikeLanesPolyLine){
+		this.BikeLanesPolyLine[l].setMap(this.mapObject);//Pintamos las lineas en el mapa	
+		}
+		LoadingDialog.hide();
+			return;
+		}
+		
+		if(this.BikeLanes!=null){//Ya tenemos los datos convertidos en el objeto jQuery pero no los hemos pintado
+		//Aquí hay que pintar las lineas recorriendo los nodos del jQuery parseado desde el XML
 			
-			bs=b;
-			MapManager.flagLanesLayer=true;
-			LoadingDialog.hide();
-		})
+			this.BikeLanes.find("LanesZone").each(function(){
+			console.log("El título de esta zona es "+$(this).children("name").text());	
+			})
+			//this.showBikeLaneLayer();
+			return;
+		}
+		console.log("Vamos a obtener los carriles");
+		
+		DataManager.getBikeLanes(function(b){
+			MapManager.BikeLanes=b;
+			//console.log("Ya estamos de vuelta");
+			MapManager.showBikeLaneLayer();
+			
+		});
+		//
+		return;
 		
 	},
 	//Esta función ocultará la capa con los carriles bici,

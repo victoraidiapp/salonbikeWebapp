1e4405efbb446efe9f552ccfc5a1472fefb8cbcc
aidiapp2013 <victor@aidiapp.com>
2014-09-27 11:15:41 +0200

    Funcion getBikeStations
    Implemento la función getBikeStations de DataManager

--- js/datamanager.js
+++ js/datamanager.js
@@ -14,7 +14,15 @@ var DataManager={
 		
 	},
 	getBikeStations:function(callBack){
-		
+		$.ajax({
+			type:"GET",
+			url:"https://clientes.domoblue.es/onroll_data/infoMarquesinas.php?key=bdedb8602218ecd22136f9546942b00e",
+			dataType:"xml",
+			success:function(xml){
+				callBack($(xml).find("ciudad"));
+	}
+	
+	})
 	}
 	
 };
